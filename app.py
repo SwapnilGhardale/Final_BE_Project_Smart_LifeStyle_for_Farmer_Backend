de31f3d3428fd155f07d457b13e911159b307408
Swapnil Ghardale <ghardaleswapnil@gmail.com>
2021-04-17 19:24:27 +0530

    updated models

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def upload():
     if request.method == 'POST':
         # Get the file from post request
         f = request.files['file']
-
+        type_of_plant=int(request.form['plant'])
         # Save the file to ./uploads
         basepath=os.path.dirname(__file__)
         file_path=os.path.join(
@@ -31,9 +31,21 @@ def upload():
         f.save(file_path)
 
         # Load model
-        model=load_model('tomato.h5',compile=False)
-        disease_class = ['Tomato_Bacterial_spot', 'Tomato_Early_blight','Tomato_Late_blight', 'Tomato_Leaf_Mold', 'Tomato_Septoria_leaf_spot','Tomato_Spider_mites_Two_spotted_spider_mite', 'Tomato__Target_Spot','Tomato__Tomato_YellowLeaf__Curl_Virus', 'Tomato__Tomato_mosaic_virus', 'Tomato_healthy']
-        
+        if(type_of_plant==1):
+            disease_class=['Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 'Tomato___healthy']
+            model = load_model('tomato.h5')
+        elif(type_of_plant==2):
+            disease_class=['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
+            model=load_model('potato.h5')
+        elif(type_of_plant==3):
+            disease_class=['Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Grape___healthy']
+            model=load_model('grape.h5')
+        elif(type_of_plant==4):
+            disease_class=['Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy']
+            model=load_model('corn(maize).h5')
+        elif(type_of_plant==5):
+            disease_class=['Strawberry___Leaf_scorch', 'Strawberry___healthy']
+            model=load_model('strawberry.h5')
         print('Model loaded. Check http://127.0.0.1:5000/')
 
         # Make prediction
